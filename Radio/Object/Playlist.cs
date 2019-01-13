73a67da6dfcda21f3bffb2fa26a6c71e26a6bc29
mrleo1nid <43583385+mrleo1nid@users.noreply.github.com>
2019-01-13 14:41:12 +0300

    Creating

---
+++ Radio/Object/Playlist.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio.Object
+{
+    class Playlist
+    {
+        private string name;
+        private string url;
+        private string imagePath;
+        private string backgroundImagePath;
+        private List<string> musicList;
+        private List<string> gifList;
+       
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                url = value;
+                OnPropertyChanged("Url");
+            }
+        }
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set
+            {
+                imagePath = value;
+                OnPropertyChanged("ImagePath");
+            }
+        }
+        public string BackgroundImagePath
+        {
+            get { return backgroundImagePath; }
+            set
+            {
+                backgroundImagePath = value;
+                OnPropertyChanged("BackgroundImagePath");
+            }
+        }
+        public List<string> MusicList
+        {
+            get { return musicList; }
+            set
+            {
+                musicList = value;
+                OnPropertyChanged("MusicList");
+            }
+        }
+        public List<string> GifList
+        {
+            get { return gifList; }
+            set
+            {
+                gifList = value;
+                OnPropertyChanged("GifList");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
---
+++ Radio/ViewModels/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Radio.Object;
+using Radio.Workers;
+
+namespace Radio.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        public ObservableCollection<Playlist> Playlists { get; set; }
+
+
+        private Playlist selectedPlaylist;
+        public Playlist SelectedPlaylist
+        {
+            get { return selectedPlaylist; }
+            set
+            {
+                selectedPlaylist = value;
+                OnPropertyChanged("SelectedPlaylist");
+            }
+        }
+
+        #region Commands
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                       (addCommand = new RelayCommand(obj =>
+                       {
+                           
+                          
+                       }));
+            }
+        }
+        #endregion
+        public MainViewModel()
+        {
+            Playlists = Worker.LoadPlaylists();
+            SelectedPlaylist = Playlists.FirstOrDefault();
+            var outer = Task.Factory.StartNew(() =>      // внешняя задача
+            {
+                for (int i = 0; i < Playlists.Count; i++)
+                {
+                    Playlists[i] = Worker.LoadImages(Playlists[i]);
+                    Thread.Sleep(5);
+                }
+               
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
